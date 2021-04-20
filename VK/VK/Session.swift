fea699d8969c1aa12dec67c8a3251ba0093bdf57
ualexeyv <ualexeyv@gmail.com>
2021-04-20 19:57:52 +0500

    connect AF

--- VK/VK/Session.swift
+++ VK/VK/Session.swift
@@ -5,13 +5,13 @@
 //  Created by пользовтель on 16.04.2021.
 //
 
-import Foundation
+import UIKit
 
 class Session {
     static let shared = Session()
     
-    private init(){}
+    private init(){ }
     
-    var token: String
-    var userId: Int
+    var token: String = "9d287439055f1d1435e843d4bf651ebaf20bac8d8d499612d157769878faa1530d6fd6fc6e5b3117966b1"
+    var userId: Int = 7831620
 }
